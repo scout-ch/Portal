6ade201adc394eb1183ddfed7e95e52d2ed2c11a
Gordon Schroeder <gordon.schroeder@itds.ch>
2020-08-27 13:42:17 +0200

    Improve table sort initialization

--- src/assets/javascripts/inc/table-sort.js
+++ src/assets/javascripts/inc/table-sort.js
@@ -2,29 +2,26 @@
   function init() {
     var tables = document.querySelectorAll('.table-sortable');
     var tLength = tables.length;
-    console.log(tables);
 
     for (var i = 0; i < tLength; i+=1) {
-      var sort = new Tablesort(tables[i]);
-      console.log(sort);
+      new Tablesort(tables[i]);
       addSortListener(tables[i]);
     }
   }
 
 
   function addSortListener(tableElement) {
-    tableElement.addEventListener('beforeSort', function onBeforeTableSort(ev) {
-      console.log('Before sort', ev);
-    });
-    tableElement.addEventListener('afterSort', function onAfterTableSort(ev) {
-      console.log('After sort', ev);
-      var orderHeading = tableElement.querySelector('th.order');
-      if (!orderHeading.getAttribute('aria-sort')) {
-        tableElement.classList.add('drag-disabled');
-      } else {
-        tableElement.classList.remove('drag-disabled');
-      }
-    });
+    var orderHeading = tableElement.querySelector('th.order');
+
+    if (orderHeading && tableElement.classList.contains('table-draggable')) {
+      tableElement.addEventListener('afterSort', function onAfterTableSort() {
+        if (!orderHeading.getAttribute('aria-sort')) {
+          tableElement.classList.add('drag-disabled');
+        } else {
+          tableElement.classList.remove('drag-disabled');
+        }
+      });
+    }
   }
 
 
