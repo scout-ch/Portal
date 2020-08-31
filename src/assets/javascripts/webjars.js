b14601273f7c3fdc16d8b7ab2daa6485b4612f98
Gordon Schroeder <gordon.schroeder@itds.ch>
2020-08-31 15:51:47 +0200

    Remove unused imports

--- src/assets/javascripts/webjars.js
+++ src/assets/javascripts/webjars.js
@@ -1,7 +1,2 @@
-//= require /webjars/jquery/3.4.1/jquery.js
-//= require /webjars/popper.js/1.16.1/dist/umd/popper.js
-//= require /webjars/bootstrap/4.5.0/js/bootstrap.js
-//= require /webjars/pnotify/4.0.0/dist/umd/PNotify.js
-//= require /webjars/pnotify/4.0.0/dist/umd/PNotifyButtons.js
 //= require /webjars/tablesort/5.2.0/dist/tablesort.min.js
 //= require /webjars/tablesort/5.2.0/dist/sorts/tablesort.number.min.js
