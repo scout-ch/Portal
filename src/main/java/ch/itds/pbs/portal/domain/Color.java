93639d36a6abd6540ae4ebcd098f428ecba456ca
Moritz Kobel <moritz.kobel@itds.ch>
2020-09-22 15:06:50 +0200

    Simplify color names

--- src/main/java/ch/itds/pbs/portal/domain/Color.java
+++ src/main/java/ch/itds/pbs/portal/domain/Color.java
@@ -3,5 +3,5 @@ package ch.itds.pbs.portal.domain;
 import ch.itds.pbs.portal.util.I18nEnum;
 
 public enum Color implements I18nEnum {
-    DEFAULT, OPTION_1, OPTION_2, OPTION_3, OPTION_4
+    DEFAULT, GREEN, BLUE, INVERTED_DEFAULT, OCHER
 }
