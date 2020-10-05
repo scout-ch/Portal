8efc1cb5caa011dee4e3703a117e8b1cd5eca0fb
Moritz Kobel <moritz.kobel@itds.ch>
2020-10-05 10:24:48 +0200

    Create category / masterTile for test if missing

--- src/test/java/ch/itds/pbs/portal/web/MasterTileControllerIntegrationTest.java
+++ src/test/java/ch/itds/pbs/portal/web/MasterTileControllerIntegrationTest.java
@@ -1,6 +1,10 @@
 package ch.itds.pbs.portal.web;
 
+import ch.itds.pbs.portal.domain.Category;
+import ch.itds.pbs.portal.domain.Color;
+import ch.itds.pbs.portal.domain.LocalizedString;
 import ch.itds.pbs.portal.domain.MasterTile;
+import ch.itds.pbs.portal.repo.CategoryRepository;
 import ch.itds.pbs.portal.repo.MasterTileRepository;
 import ch.itds.pbs.portal.web.page.admin.MasterTileCreatePage;
 import ch.itds.pbs.portal.web.page.admin.MasterTileEditPage;
@@ -8,12 +12,17 @@ import ch.itds.pbs.portal.web.page.admin.MasterTileIndexPage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MasterTileControllerIntegrationTest extends IntegrationTest {
 
     @Autowired
     MasterTileRepository masterTileRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @Test
     public void testIndex() throws InterruptedException {
@@ -59,7 +68,37 @@ public class MasterTileControllerIntegrationTest extends IntegrationTest {
     @Test
     public void testEdit() throws InterruptedException {
 
-        MasterTile masterTile = masterTileRepository.findAll().get(0);
+        List<MasterTile> masterTiles = masterTileRepository.findAll();
+        MasterTile masterTile;
+        if (masterTiles.isEmpty()) {
+            Category category;
+            List<Category> categories = categoryRepository.findAll();
+            if (categories.isEmpty()) {
+                LocalizedString categoryName = new LocalizedString();
+                categoryName.setDe("Kategorie");
+
+                category = new Category();
+                category.setName(categoryName);
+                category = categoryRepository.save(category);
+            } else {
+                category = categories.get(0);
+            }
+
+            LocalizedString title = new LocalizedString();
+            title.setDe("Titel");
+            LocalizedString content = new LocalizedString();
+            content.setDe("Content...");
+
+            masterTile = new MasterTile();
+            masterTile.setCategory(category);
+            masterTile.setTitle(title);
+            masterTile.setContent(content);
+            masterTile.setBackgroundColor(Color.DEFAULT);
+            masterTile = masterTileRepository.saveAndFlush(masterTile);
+        } else {
+            masterTile = masterTiles.get(0);
+        }
+
 
         MasterTileEditPage editPage = MasterTileEditPage.open(seleniumHelper, masterTile.getId());
 
