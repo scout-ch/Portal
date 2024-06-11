d8070b9d0143ed1757dadf85fbf5447751bf1a77
Katrin Gfeller <katrin.gfeller@qube.ag>
2024-06-11 08:34:25 +0200

    corrections

--- src/main/java/ch/itds/pbs/portal/service/UserService.java
+++ src/main/java/ch/itds/pbs/portal/service/UserService.java
@@ -31,42 +31,4 @@ public class UserService {
         Language language = Language.valueOfOrDefault(locale.getLanguage().toUpperCase());
         userRepository.setLanguage(userPrincipal.getId(), language);
     }
-
-    public void updateRoles(String username, List<String> roleNames) {
-        Optional<User> userOptional = userRepository.findByUsername(username);
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            Set<Role> roles = new HashSet<>();
-            for (String roleNameShort : roleNames) {
-                String roleName = "ROLE_" + roleNameShort;
-                Optional<Role> optRole = roleRepository.findByName(roleName);
-                if (optRole.isPresent()) {
-                    roles.add(optRole.get());
-                } else {
-                    Role role = new Role();
-                    role.setName(roleName);
-                    roleRepository.save(role);
-                    roles.add(role);
-                }
-            }
-            user.setRoles(roles);
-            try {
-                userRepository.save(user);
-            } catch (ObjectOptimisticLockingFailureException oollfe) {
-                log.info("unable to save user {}/{}: {}", user.getMidataUserId(), user.getMail(), oollfe.getMessage());
-            }
-        }
-    }
-
-    public List<String> extractRoleNames(ClaimAccessor principal) {
-        List<String> list = new ArrayList<>();
-        if (principal.hasClaim("crs-intranet-roles")) {
-            for (String rawRole : principal.getClaimAsStringList("crs-intranet-roles")) {
-                String cleanRole = rawRole.replaceAll("([a-z])([A-Z])", "$1_$2").toUpperCase();
-                list.add(cleanRole);
-            }
-
-        }
-        return list;
-    }
 }
