6f4bd808307a395b93edf3de793ebb855a674fb3
Katrin Gfeller <katrin.gfeller@qube.ag>
2024-06-11 08:39:25 +0200

    corrections

--- src/main/java/ch/itds/pbs/portal/service/UserService.java
+++ src/main/java/ch/itds/pbs/portal/service/UserService.java
@@ -1,14 +1,9 @@
 package ch.itds.pbs.portal.service;
 
 import ch.itds.pbs.portal.domain.Language;
-import ch.itds.pbs.portal.domain.Role;
-import ch.itds.pbs.portal.domain.User;
-import ch.itds.pbs.portal.repo.RoleRepository;
 import ch.itds.pbs.portal.repo.UserRepository;
 import ch.itds.pbs.portal.security.UserPrincipal;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.orm.ObjectOptimisticLockingFailureException;
-import org.springframework.security.oauth2.core.ClaimAccessor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,11 +14,9 @@ import java.util.*;
 public class UserService {
 
     private final transient UserRepository userRepository;
-    private final transient RoleRepository roleRepository;
 
-    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
     }
 
     @Transactional
